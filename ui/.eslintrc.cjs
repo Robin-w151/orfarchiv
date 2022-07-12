98329ddd99281dba88abe06e8506e44f38de2ebd
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2022-07-12 08:26:38 +0200

    setup ui webapp

---
+++ ui/.eslintrc.cjs
@@ -0,0 +1,44 @@
+const prettier = require('./.prettierrc.json');
+
+module.exports = {
+	root: true,
+	rules: {
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/no-var-requires': 'off',
+		'@typescript-eslint/no-unused-vars': 'warn',
+		'no-unused-vars': 'off',
+		'prettier/prettier': [
+			'error',
+			{
+				...prettier,
+			},
+		],
+	},
+	extends: ['plugin:@typescript-eslint/recommended', 'plugin:svelte/recommended', 'plugin:prettier/recommended'],
+	plugins: ['svelte3', '@typescript-eslint'],
+	env: {
+		browser: true,
+		es2022: true,
+		node: true
+	},
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		sourceType: 'module',
+		ecmaVersion: 2022,
+		tsconfigRootDir: __dirname,
+		extraFileExtensions: ['.svelte'],
+	},
+	// overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
+	overrides: [
+		{
+			files: ['*.svelte'],
+			parser: 'svelte-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser',
+			},
+		},
+	],
+	// settings: {
+	// 	'svelte3/typescript': () => require('typescript')
+	// }
+};
