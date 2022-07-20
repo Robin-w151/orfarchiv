ae7dc5eb3aa50cdc3cfbffbdcec9e598f1babeda
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2022-07-20 17:10:40 +0200

    update control styles

--- ui/src/lib/components/ui/controls/Button.svelte
+++ ui/src/lib/components/ui/controls/Button.svelte
@@ -3,7 +3,12 @@
 
   export let type: 'button' | 'reset' | 'submit' = 'button';
 
-  const buttonClass = classNames(['px-3 py-2', 'text-white bg-blue-800 hover:bg-blue-700', 'rounded-lg']);
+  const buttonClass = classNames([
+    'px-3 py-2',
+    'text-white bg-blue-800 hover:bg-blue-700',
+    'outline-hidden focus:outline outline-2 outline-offset-2 outline-blue-700',
+    'rounded-lg',
+  ]);
 </script>
 
 <button class={buttonClass} {type} on:click>
--- ui/src/lib/components/ui/controls/ButtonLink.svelte
+++ ui/src/lib/components/ui/controls/ButtonLink.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
+  import classNames from 'classnames';
+
   export let href: string = undefined;
   export let target: string = undefined;
+
+  const buttonClass = classNames([
+    'p-2',
+    'text-blue-800 hover:text-blue-700 hover:bg-gray-100',
+    'outline-hidden focus:outline outline-2 outline-blue-700',
+    'rounded-md hover:shadow-lg',
+  ]);
 </script>
 
 {#if href}
-  <a {href} {target}>
+  <a class={buttonClass} {href} {target}>
     <slot />
   </a>
 {:else}
-  <button on:click><slot /></button>
+  <button class={buttonClass} on:click><slot /></button>
 {/if}
