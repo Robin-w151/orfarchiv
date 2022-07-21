80a00bf7b168bdfa604652c41a297f8f2bad16ee
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2022-07-21 17:11:33 +0200

    update run script

--- scraper/run.sh
+++ scraper/run.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 dbUrlFile=$1
-if [ -z "$dbUrlFile" ]; then
-    echo 'usage: ./run <DB-URL-FILE>'
-    exit 1
-fi
 
-ORFARCHIV_DB_URL=$(cat "$dbUrlFile" 2> /dev/null)
-export ORFARCHIV_DB_URL
+if [ -n "$dbUrlFile" ]; then
+  ORFARCHIV_DB_URL=$(cat "$dbUrlFile" 2> /dev/null)
+  export ORFARCHIV_DB_URL
+fi
 
 timeout 60s yarn start
