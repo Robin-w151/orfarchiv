21686baabbc43ec18699b959bcac77d36099ecd5
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2022-07-21 16:49:06 +0200

    add db url config option

--- ui/src/lib/db/news.ts
+++ ui/src/lib/db/news.ts
@@ -3,11 +3,9 @@ import type { News } from '../models/news';
 import type { Story } from '../models/story';
 import { DateTime } from 'luxon';
 
-const rssOrfUrl = 'mongodb://localhost';
-
 export async function findNews(maxAgeInMin: number): Promise<News> {
   const earliestTimestamp = DateTime.now().minus({ minutes: maxAgeInMin }).toISO();
-  return withOrfArchivDb(rssOrfUrl, async (newsCollection) => {
+  return withOrfArchivDb(async (newsCollection) => {
     const stories = await newsCollection
       .find({ timestamp: { $gte: new Date(earliestTimestamp) } })
       .sort({ timestamp: -1 })
@@ -16,7 +14,8 @@ export async function findNews(maxAgeInMin: number): Promise<News> {
   });
 }
 
-async function withOrfArchivDb(url: string, handler: (newsCollection: Collection) => Promise<News>) {
+async function withOrfArchivDb(handler: (newsCollection: Collection) => Promise<News>) {
+  const url = process.env.ORFARCHIV_DB_URL?.trim() || 'mongodb://localhost';
   let client;
   try {
     client = await MongoClient.connect(url);
