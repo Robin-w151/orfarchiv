012b69b5b025db7da456f81f55084eae8ebcf4c8
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2022-07-22 16:10:55 +0200

    update caching strategy

--- ui/src/routes/news/index.ts
+++ ui/src/routes/news/index.ts
@@ -7,6 +7,7 @@ export async function get() {
     status: 200,
     headers: {
       'Content-Type': 'application/json',
+      'Cache-Control': 'max-age=60, public',
     },
     body: news,
   };
