e57afb785349025b367638f8bdc8f76ff00a9279
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2022-07-23 11:12:21 +0200

    remove server module (obsolete)

--- server/src/scraper/models/story.ts
+++
@@ -1,7 +0,0 @@
-export interface Story {
-  id: string;
-  title: string;
-  category: string;
-  url: string;
-  timestamp: string;
-}
--- server/src/scraper/scraper.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ScraperService } from './scraper.service';
-import { HttpModule } from '@nestjs/axios';
-
-@Module({
-  imports: [HttpModule],
-  providers: [ScraperService],
-})
-export class ScraperModule {}
