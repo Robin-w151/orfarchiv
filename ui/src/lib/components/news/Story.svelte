e919be031c9ed14016a0c63dcf895a5f1c65ef39
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2022-07-23 15:54:29 +0200

    add source label

--- ui/src/lib/components/news/Story.svelte
+++ ui/src/lib/components/news/Story.svelte
@@ -4,24 +4,36 @@
   import Item from '../ui/content/Item.svelte';
   import settings from '../../stores/settings';
   import getCategoryColorClass from '../../utils/categoryColorPalette';
+  import { sources } from '../../models/settings';
 
   export let title: string;
   export let category: string;
   export let url: string;
   export let timestamp: string;
+  export let source: string;
 
   $: categoryColor = $settings.useCategoryColorPalette ? getCategoryColorClass(category) : null;
   $: target = $settings.openLinksInNewTab ? '_blank' : null;
+  $: sourceLabel = getSourceLabel(source);
 
   const titleClass = classNames();
-  const dateClass = classNames(['text-sm', 'text-gray-600']);
+  const metadataClass = classNames(['text-sm', 'text-gray-600']);
 
   function formatTimestamp(timestamp: string): string {
     return DateTime.fromISO(timestamp).toFormat('dd.MM.yyyy, HH:mm');
+  }
+
+  function getSourceLabel(source: string): string {
+    if (!source || source === 'news') {
+      return undefined;
+    }
+    return sources.find((s) => s.key === source)?.label;
   }
 </script>
 
 <Item href={url} {target} {categoryColor}>
   <span class={titleClass}>{title}</span>
-  <span class={dateClass}>{category} - {formatTimestamp(timestamp)}</span>
+  <span class={metadataClass}
+    >{#if sourceLabel}<span>{sourceLabel} - </span>{/if}{category} - {formatTimestamp(timestamp)}</span
+  >
 </Item>
