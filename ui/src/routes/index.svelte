1471874d6dcd4c1bd97a9d57a9cd0803b52dbd06
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2022-07-26 21:57:31 +0200

    disable reload when navigating to index page and stories do already exist

--- ui/src/routes/index.svelte
+++ ui/src/routes/index.svelte
@@ -2,11 +2,15 @@
   import News from '../lib/components/news/News.svelte';
   import news from '../lib/stores/news';
   import { getNews } from '../lib/api/news';
+  import { get } from 'svelte/store';
 
   type FetchFn = (input: RequestInfo | URL, init?: RequestInit) => Promise<Response>;
 
   export async function load({ fetch }: { fetch: FetchFn }) {
-    news.setNews(await getNews(fetch));
+    const stories = get(news)?.stories?.length ?? 0;
+    if (stories === 0) {
+      news.setNews(await getNews(fetch));
+    }
     return {
       props: {},
     };
