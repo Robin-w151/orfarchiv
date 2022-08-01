dc32ef86c42a31019ac2e940d215313e82de399b
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2022-08-01 16:42:02 +0200

    remove favicon/robots.txt; add sitemap

---
+++ ui/src/routes/sitemap.xml.ts
@@ -0,0 +1,25 @@
+/** @type {import('@sveltejs/kit').RequestHandler} */
+export async function get() {
+  return {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/xml',
+    },
+    body: `
+      <?xml version="1.0" encoding="UTF-8" ?>
+      <urlset
+        xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:xhtml="https://www.w3.org/1999/xhtml"
+        xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
+        xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
+        xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
+        xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
+      >
+        <url>
+          <loc>https://orfarchiv.news</loc>
+          <lastmod>2022-08-01T16:35:00+02:00</lastmod>
+        </url>
+      </urlset>
+    `.trim(),
+  };
+}
