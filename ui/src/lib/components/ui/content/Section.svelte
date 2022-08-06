124580a9cb96cfd56887f9d2b34b1eda043db0c0
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2022-08-06 21:41:00 +0200

    adjust section title style

--- ui/src/lib/components/ui/content/Section.svelte
+++ ui/src/lib/components/ui/content/Section.svelte
@@ -5,17 +5,21 @@
 
   const sectionClass = classNames(['flex flex-col items-center', 'w-full']);
   const titleClass = classNames([
-    'flex justify-center sticky top-2',
-    'mb-3 px-3 py-2',
-    'w-48 sm:w-64 max-w-full',
+    'flex justify-center sticky top-0',
+    'px-3 sm:px-6 py-3',
+    'w-full text-lg',
     'text-blue-900 bg-white',
-    'rounded-lg shadow-md',
+    'border-solid border-b-2',
+    'rounded-t-lg',
   ]);
+  const contentClass = classNames(['w-full', 'overflow-hidden', 'rounded-b-lg']);
 </script>
 
 <div class={sectionClass}>
   {#if title}
     <h2 class={titleClass}>{title}</h2>
   {/if}
-  <slot />
+  <div class={contentClass}>
+    <slot />
+  </div>
 </div>
--- ui/src/lib/components/ui/content/SectionList.svelte
+++ ui/src/lib/components/ui/content/SectionList.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import classNames from 'classnames';
 
-  const listClass = classNames(['w-full', 'overflow-hidden', 'bg-white', 'divide-y-2 divide-solid', 'rounded-lg']);
+  const listClass = classNames(['w-full', 'bg-white', 'divide-y-2 divide-solid']);
 </script>
 
 <ul class={listClass}>
