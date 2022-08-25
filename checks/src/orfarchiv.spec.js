5462ce51ea30a8903d1ac5f6d785a2a8fe191b35
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2022-08-25 15:30:06 +0200

    add eslint/prettier

--- checks/src/orfarchiv.spec.js
+++ checks/src/orfarchiv.spec.js
@@ -1,19 +1,19 @@
-const { chromium } = require("playwright")
+const { chromium } = require('playwright');
 
 const main = async () => {
-  const browser = await chromium.launch()
-  const page = await browser.newPage()
+  const browser = await chromium.launch();
+  const page = await browser.newPage();
 
-  const targetUrl = process.env.ENVIRONMENT_URL || "https://orfarchiv.news"
-  const response = await page.goto(targetUrl)
+  const targetUrl = process.env.ENVIRONMENT_URL || 'https://orfarchiv.news';
+  const response = await page.goto(targetUrl);
 
   if (response.status() > 399) {
-    throw new Error(`Failed with response code ${response.status()}`)
+    throw new Error(`Failed with response code ${response.status()}`);
   }
 
-  await page.screenshot({ path: "screenshot-orfarchiv.jpg" })
-  await page.close()
-  await browser.close()
-}
+  await page.screenshot({ path: 'screenshot-orfarchiv.jpg' });
+  await page.close();
+  await browser.close();
+};
 
-main()
+main();
