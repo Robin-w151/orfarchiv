0a8277e6d2333245fbec7609a9e08bd8f2b6a593
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2022-09-01 15:07:32 +0200

    add run script

---
+++ db/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+
+scriptDir=$(dirname "$0")
+dbUrlFile="$1"
+backupDirFile="$2"
+
+if [ -n "$dbUrlFile" ]; then
+  ORFARCHIV_DB_URL=$(cat "$dbUrlFile" 2> /dev/null)
+  export ORFARCHIV_DB_URL
+fi
+
+if [ -n "$backupDirFile" ]; then
+  ORFARCHIV_BACKUP_DIR=$(cat "$backupDirFile" 2> /dev/null)
+  export ORFARCHIV_BACKUP_DIR
+fi
+
+cd "$scriptDir"
+timeout 60s yarn start
