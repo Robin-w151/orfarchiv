11b37ac426bf27637046b6668dcb0b50bf0c7858
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2022-09-01 14:53:14 +0200

    add backup script

---
+++ db/src/index.js
@@ -0,0 +1,51 @@
+const logger = require('./logger');
+const { MongoClient } = require('mongodb');
+const { join } = require('path');
+const { writeFile, mkdir } = require('fs').promises;
+require('dotenv-flow').config();
+
+main();
+
+async function main() {
+  try {
+    await exportNews();
+  } catch (error) {
+    logger.error(error.message);
+  }
+}
+
+async function exportNews() {
+  const news = await withOrfArchivDb(async (newsCollection) => {
+    logger.info('Fetching data...');
+    return await newsCollection.find().toArray();
+  });
+
+  logger.info('Persisting data to backup file...');
+  const timestamp = getTimestamp();
+  const backupPath = join(process.env.ORFARCHIV_BACKUP_DIR || '.backup');
+  await mkdir(backupPath, { recursive: true });
+  const backupFilePath = join(backupPath, `${timestamp}.json`);
+  await writeFile(backupFilePath, JSON.stringify(news), { flag: 'w' });
+}
+
+async function withOrfArchivDb(handler) {
+  logger.info('Connecting to DB...');
+  const url = process.env.ORFARCHIV_DB_URL?.trim() || 'mongodb://localhost';
+  let client;
+  try {
+    client = await MongoClient.connect(url);
+    const db = client.db('orfarchiv');
+    const newsCollection = db.collection('news');
+    return await handler(newsCollection);
+  } catch (error) {
+    throw new Error(`DB error. Cause ${error.message}`);
+  } finally {
+    await client?.close();
+  }
+}
+
+function getTimestamp() {
+  const now = new Date();
+  const nowString = now.toISOString();
+  return nowString.replaceAll(':', '').split('.')[0] + 'Z';
+}
