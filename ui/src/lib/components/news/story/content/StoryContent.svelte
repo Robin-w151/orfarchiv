aee288501b42fc910ddf9457dd56bc16be3f0e6f
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2022-09-08 20:22:27 +0200

    fix content width

--- ui/src/lib/components/news/story/content/StoryContent.svelte
+++ ui/src/lib/components/news/story/content/StoryContent.svelte
@@ -18,7 +18,7 @@
   let isClosed = false;
 
   const wrapperClass = classNames('flex flex-col items-center gap-3');
-  const contentClass = classNames('cursor-auto');
+  const contentClass = classNames('cursor-auto w-full');
   const contentSourceClass = classNames('text-sm text-gray-600');
   const errorLinkClass = classNames(['text-blue-800']);
   const collapseContentClass = classNames(['py-1.5 w-48 max-w-full']);
