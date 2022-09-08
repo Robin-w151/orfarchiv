45f81ce675e08df0cd1739e7dd0a24b01f9a9d2f
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2022-09-08 20:14:40 +0200

    adjust check config

--- checks/playwright.config.js
+++ checks/playwright.config.js
@@ -1,8 +1,12 @@
-require('dotenv-flow').config();
+require('dotenv-flow').config({ silent: true });
 
 module.exports = {
+  timeout: 10000,
+  globalTimeout: 600000,
+  retries: 2,
   use: {
     headless: true,
+    browserName: 'chromium',
     viewport: { width: 1000, height: 900 },
     testDir: './tests',
   },
