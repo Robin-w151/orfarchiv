47bb63ec9c5cb9bbc423b7c40235f2b5bc65caab
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2022-10-06 17:07:02 +0200

    extract header into separate component

---
+++ ui/src/lib/components/ui/content/Header.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import { defaultPadding } from '$lib/utils/styles';
+  import { refreshNews } from '$lib/stores/newsEvents';
+  import CogIcon from '$lib/components/ui/icons/outline/CogIcon.svelte';
+  import RefreshIcon from '$lib/components/ui/icons/outline/RefreshIcon.svelte';
+  import ButtonLink from '$lib/components/ui/controls/ButtonLink.svelte';
+  import TextGradient from '$lib/components/ui/content/TextGradient.svelte';
+
+  const headerClass = `
+    flex justify-between items-center gap-6
+    ${defaultPadding}
+    text-3xl
+    text-blue-700 bg-white
+  `;
+  const headerTitleClass = 'focus:bg-blue-100 outline-none rounded-md';
+  const headerActionsClass = 'flex gap-2';
+
+  function handleRefreshButtonClick() {
+    refreshNews.notify();
+  }
+</script>
+
+<header class={headerClass}>
+  <h1>
+    <a class={headerTitleClass} href="/" title="Startseite" data-sveltekit-prefetch>
+      <TextGradient>ORF Archiv</TextGradient>
+    </a>
+  </h1>
+  <div class={headerActionsClass}>
+    <ButtonLink href="/" title="Nach Updates suchen" iconOnly on:click={handleRefreshButtonClick} preventDefault>
+      <RefreshIcon />
+    </ButtonLink>
+    <ButtonLink href="/settings" title="Einstellungen" iconOnly prefetch>
+      <CogIcon />
+    </ButtonLink>
+  </div>
+</header>
--- ui/src/routes/+layout.svelte
+++ ui/src/routes/+layout.svelte
@@ -1,14 +1,9 @@
 <script lang="ts">
   import '../app.scss';
-  import CogIcon from '$lib/components/ui/icons/outline/CogIcon.svelte';
-  import RefreshIcon from '$lib/components/ui/icons/outline/RefreshIcon.svelte';
-  import ButtonLink from '$lib/components/ui/controls/ButtonLink.svelte';
-  import { refreshNews } from '$lib/stores/newsEvents';
   import { webVitals } from '$lib/utils/vitals';
   import { browser } from '$app/environment';
   import { page } from '$app/stores';
-  import { defaultPadding } from '$lib/utils/styles';
-  import TextGradient from '$lib/components/ui/content/TextGradient.svelte';
+  import Header from '$lib/components/ui/content/Header.svelte';
 
   const analyticsId = import.meta.env.VERCEL_ANALYTICS_ID;
   $: if (browser && analyticsId) {
@@ -24,37 +19,11 @@
     p-2 pb-4 sm:p-4
     w-screen max-w-screen-lg
   `;
-  const headerClass = `
-    flex justify-between items-center gap-6
-    ${defaultPadding}
-    text-3xl
-    text-blue-700 bg-white
-  `;
-  const headerTitleClass = 'focus:bg-blue-100 outline-none rounded-md';
-  const headerActionsClass = 'flex gap-2';
   const mainClass = 'flex flex-col gap-2 sm:gap-3';
-
-  function handleRefreshButtonClick() {
-    refreshNews.notify();
-  }
 </script>
 
 <div class={wrapperClass}>
-  <header class={headerClass}>
-    <h1>
-      <a class={headerTitleClass} href="/" title="Startseite" data-sveltekit-prefetch>
-        <TextGradient>ORF Archiv</TextGradient>
-      </a>
-    </h1>
-    <div class={headerActionsClass}>
-      <ButtonLink href="/" title="Nach Updates suchen" iconOnly on:click={handleRefreshButtonClick} preventDefault>
-        <RefreshIcon />
-      </ButtonLink>
-      <ButtonLink href="/settings" title="Einstellungen" iconOnly prefetch>
-        <CogIcon />
-      </ButtonLink>
-    </div>
-  </header>
+  <Header />
   <main class={mainClass}>
     <slot />
   </main>
