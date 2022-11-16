08d57f5ee2de2e2132122e87ca5c29e10cff458a
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2022-11-16 21:23:16 +0100

    switch to localStorage

--- ui/src/lib/stores/readLater.ts
+++ ui/src/lib/stores/readLater.ts
@@ -1,24 +1,15 @@
 import type { ReadLater } from '$lib/models/readLater';
 import type { Story } from '$lib/models/story';
 import { DateTime } from 'luxon';
-import { writable, type Readable } from 'svelte/store';
-import ReadLaterDB from './persistence/readLaterDB';
+import { writable } from 'svelte-local-storage-store';
+import type { Readable } from 'svelte/store';
 
 export interface ReadLaterStore extends Readable<ReadLater>, Partial<ReadLater> {
   addStory: (story: Story) => void;
 }
 
-const db = new ReadLaterDB();
-
 const initialState = { stories: [] };
-const { subscribe, update } = writable<ReadLater>(initialState);
-init();
-
-async function init(): Promise<void> {
-  await db.init();
-  const stories = (await db.findAll()).sort(compareStories);
-  update((readLater) => ({ ...readLater, stories }));
-}
+const { subscribe, update } = writable<ReadLater>('readLater', initialState);
 
 function addStory(story: Story): void {
   update((readLater) => ({ ...readLater, stories: insertIfAbsent(story, readLater.stories) }));
@@ -30,7 +21,6 @@ function insertIfAbsent(story: Story, stories: Array<Story>): Array<Story> {
   let newStories = stories;
   if (isAbsent) {
     newStories = [story, ...stories];
-    db.persist(story);
   }
 
   return newStories.sort(compareStories);
