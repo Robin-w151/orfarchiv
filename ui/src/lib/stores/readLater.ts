1efb929288de2ad21842bc26834d6538cc04d173
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2022-11-16 16:50:45 +0100

    load readLater stories on store init

--- ui/src/lib/stores/readLater.ts
+++ ui/src/lib/stores/readLater.ts
@@ -8,16 +8,22 @@ export interface ReadLaterStore extends Readable<ReadLater>, Partial<ReadLater> 
 }
 
 const db = new ReadLaterDB();
-db.init();
 
 const initialState = { stories: [] };
 const { subscribe, update } = writable<ReadLater>(initialState);
+init();
+
+async function init(): Promise<void> {
+  await db.init();
+  const stories = await db.findAll();
+  update((readLater) => ({ ...readLater, stories }));
+}
 
 function addStory(story: Story): void {
   update((readLater) => ({ ...readLater, stories: insertIfAbsent(story, readLater.stories) }));
 }
 
-function insertIfAbsent(story: Story, stories: Array<Story>) {
+function insertIfAbsent(story: Story, stories: Array<Story>): void {
   const isAbsent = !stories.find((s) => s.id === story.id);
 
   let newStories = stories;
