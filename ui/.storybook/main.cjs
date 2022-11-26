04b41c5dc9e2ff9386427db8b6456da4a4157e3b
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2022-11-26 11:55:07 +0100

    add tailwind dark mode addon

--- ui/.storybook/main.cjs
+++ ui/.storybook/main.cjs
@@ -2,7 +2,12 @@ const preprocess = require('svelte-preprocess');
 
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    'storybook-tailwind-dark-mode',
+  ],
   framework: '@storybook/svelte',
   core: {
     builder: '@storybook/builder-vite',
