20ea6afd46eb9d65e801b49fcadfdfa8d3d62d19
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2022-12-08 20:39:54 +0100

    add setup script

---
+++ db/src/setup.js
@@ -0,0 +1,37 @@
+const logger = require('./logger');
+const { MongoClient } = require('mongodb');
+
+main();
+
+async function main() {
+  logger.info('Connecting to server...');
+  const url = process.env.ORFARCHIV_DB_URL?.trim() || 'mongodb://localhost';
+  const client = await MongoClient.connect(url);
+
+  logger.info('Creating orfarchiv DB...');
+  const db = client.db('orfarchiv');
+
+  logger.info('Creating news collection...');
+  const collections = await db.collections();
+  if (!collections.find((collection) => collection.collectionName === 'news')) {
+    await db.createCollection('news');
+    const news = db.collection('news');
+    await news.createIndexes([
+      {
+        id: 1,
+      },
+      {
+        id: -1,
+      },
+      {
+        timestamp: 1,
+      },
+      {
+        timestamp: -1,
+      },
+    ]);
+  }
+
+  await client.close();
+  logger.info('Done.');
+}
