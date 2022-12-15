3e89c56469c78797360ba81859083da004f1d463
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2022-12-15 17:05:04 +0100

    add storybook 7

---
+++ ui/.storybook/main.cjs
@@ -0,0 +1,19 @@
+module.exports = {
+  'stories': [
+    '../src/**/*.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
+  'addons': [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    'storybook-tailwind-dark-mode',
+  ],
+  'framework': {
+    'name': '@storybook/sveltekit',
+    'options': {},
+  },
+  'docs': {
+    'docsPage': true,
+  },
+};
