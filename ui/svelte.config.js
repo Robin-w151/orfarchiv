36b20e0c4201ca37a956998cc2ec701603dd3cd9
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2023-01-08 14:24:59 +0100

    add disable csp option for local development

--- ui/svelte.config.js
+++ ui/svelte.config.js
@@ -2,9 +2,9 @@ import adapterNode from '@sveltejs/adapter-node';
 import adapterVercel from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
-const useAdapterNode = process.env.USE_ADAPTER_NODE;
+const useAdapterNode = process.env.USE_ADAPTER_NODE === 'true';
+const disableCsp = process.env.DISABLE_CSP === 'true';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
@@ -13,16 +13,18 @@ const config = {
   }),
   kit: {
     adapter: useAdapterNode ? adapterNode() : adapterVercel(),
-    csp: {
-      directives: {
-        'default-src': ['none'],
-        'connect-src': ['self', 'https://vitals.vercel-analytics.com'],
-        'font-src': ['self', 'data:'],
-        'img-src': ['*', 'data:'],
-        'script-src': ['self', 'sha256-yzyqz6goBMMs4IDXDK6aJMrqy/rGVe0pmG0JBf6EgTQ='],
-        'style-src': ['self', 'unsafe-inline'],
-      },
-    },
+    csp: disableCsp
+      ? undefined
+      : {
+          directives: {
+            'default-src': ['none'],
+            'connect-src': ['self', 'https://vitals.vercel-analytics.com'],
+            'font-src': ['self', 'data:'],
+            'img-src': ['*', 'data:'],
+            'script-src': ['self', 'sha256-yzyqz6goBMMs4IDXDK6aJMrqy/rGVe0pmG0JBf6EgTQ='],
+            'style-src': ['self', 'unsafe-inline'],
+          },
+        },
   },
 };
 
