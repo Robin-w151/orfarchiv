4de53bb370da23e4564780044bb50cbd3bb91343
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2023-01-14 16:14:48 +0100

    switch to vitePreprocess

--- ui/svelte.config.js
+++ ui/svelte.config.js
@@ -1,30 +1,33 @@
 import adapterNode from '@sveltejs/adapter-node';
 import adapterVercel from '@sveltejs/adapter-vercel';
-import preprocess from 'svelte-preprocess';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 const useAdapterNode = process.env.USE_ADAPTER_NODE === 'true';
 const disableCsp = process.env.DISABLE_CSP === 'true';
 
+const adapter = useAdapterNode ? adapterNode() : adapterVercel();
+const csp = disableCsp
+  ? undefined
+  : {
+      directives: {
+        'default-src': ['none'],
+        'connect-src': ['self', 'https://vitals.vercel-analytics.com'],
+        'font-src': ['self', 'data:'],
+        'img-src': ['*', 'data:'],
+        'script-src': ['self', 'sha256-yzyqz6goBMMs4IDXDK6aJMrqy/rGVe0pmG0JBf6EgTQ='],
+        'style-src': ['self', 'unsafe-inline'],
+      },
+    };
+
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
-  preprocess: preprocess({
-    postcss: true,
-  }),
+  preprocess: [
+    vitePreprocess({
+      postcss: true,
+    }),
+  ],
   kit: {
-    adapter: useAdapterNode ? adapterNode() : adapterVercel(),
-    csp: disableCsp
-      ? undefined
-      : {
-          directives: {
-            'default-src': ['none'],
-            'connect-src': ['self', 'https://vitals.vercel-analytics.com'],
-            'font-src': ['self', 'data:'],
-            'img-src': ['*', 'data:'],
-            'script-src': ['self', 'sha256-yzyqz6goBMMs4IDXDK6aJMrqy/rGVe0pmG0JBf6EgTQ='],
-            'style-src': ['self', 'unsafe-inline'],
-          },
-        },
+    adapter,
+    csp,
   },
 };
 
