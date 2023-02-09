15a53928844e37ea5f61a1858cfa1a300bde85fe
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2023-02-09 09:26:18 +0100

    return 404 on favicon request

--- ui/src/routes/+error.svelte
+++ ui/src/routes/+error.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
-  import { redirect } from '@sveltejs/kit';
+  import { page } from '$app/stores';
+  import { error, redirect } from '@sveltejs/kit';
+
+  const path = $page.url.pathname;
+  if (path === '/favicon.ico') {
+    throw error(404);
+  }
 
   throw redirect(302, '/');
 </script>
