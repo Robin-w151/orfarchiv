9dc211c9169b88a4288d0daf25c6aef7cb769d25
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2023-02-14 21:53:32 +0100

    calculate script hashes dynamically

--- ui/svelte.config.js
+++ ui/svelte.config.js
@@ -1,6 +1,9 @@
 import adapterNode from '@sveltejs/adapter-node';
 import adapterVercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import { readFile } from 'fs/promises';
+import { JSDOM } from 'jsdom';
+import crypto from 'crypto';
 
 const useAdapterNode = process.env.USE_ADAPTER_NODE === 'true';
 const disableCsp = process.env.DISABLE_CSP === 'true';
@@ -14,7 +17,7 @@ const csp = disableCsp
         'connect-src': ['self', 'https://vitals.vercel-analytics.com'],
         'font-src': ['self', 'data:'],
         'img-src': ['*', 'data:'],
-        'script-src': ['self', 'sha256-yzyqz6goBMMs4IDXDK6aJMrqy/rGVe0pmG0JBf6EgTQ='],
+        'script-src': ['self', ...(await calculateScriptHashes())],
         'style-src': ['self', 'unsafe-inline'],
       },
     };
@@ -30,5 +33,24 @@ const config = {
     csp,
   },
 };
+
+async function calculateScriptHashes() {
+  const decoder = new TextDecoder('utf-8');
+  const appHtml = decoder.decode(await readFile('src/app.html'));
+
+  const appDocument = new JSDOM(appHtml).window.document;
+  const appearanceScript = appDocument.getElementsByTagName('script')[0];
+  const appearanceScriptHash = await calculateScriptHash(appearanceScript.textContent);
+
+  return [appearanceScriptHash];
+}
+
+async function calculateScriptHash(scriptText) {
+  const textEncoder = new TextEncoder('utf-8').encode(scriptText);
+  const hashBuffer = await crypto.subtle.digest('SHA-256', textEncoder);
+  const hashString = Buffer.from(hashBuffer).toString('base64');
+
+  return `sha256-${hashString}`;
+}
 
 export default config;
