abe712e7665a2a591b2057fdf14f78d92864232b
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2023-05-03 15:54:17 +0000

    remove wrong at-rule usage

--- ui/src/lib/components/ui/loading/ConicGradientSpinner.svelte
+++ ui/src/lib/components/ui/loading/ConicGradientSpinner.svelte
@@ -10,27 +10,23 @@
 <div class="spinner {spinnerClass}" />
 
 <style lang="postcss">
-  @tailwind components;
+  .spinner {
+    background: conic-gradient(
+      transparent 0deg,
+      theme('colors.fuchsia.600') 90deg,
+      theme('colors.violet.600') 180deg,
+      theme('colors.blue.700') 270deg,
+      transparent 355deg
+    );
+  }
 
-  @layer components {
-    .spinner {
-      background: conic-gradient(
-        transparent 0deg,
-        theme('colors.fuchsia.600') 90deg,
-        theme('colors.violet.600') 180deg,
-        theme('colors.blue.700') 270deg,
-        transparent 355deg
-      );
-    }
-
-    :global(.dark) .spinner {
-      background: conic-gradient(
-        transparent 0deg,
-        theme('colors.fuchsia.400') 90deg,
-        theme('colors.violet.500') 180deg,
-        theme('colors.blue.500') 270deg,
-        transparent 355deg
-      );
-    }
+  :global(.dark) .spinner {
+    background: conic-gradient(
+      transparent 0deg,
+      theme('colors.fuchsia.400') 90deg,
+      theme('colors.violet.500') 180deg,
+      theme('colors.blue.500') 270deg,
+      transparent 355deg
+    );
   }
 </style>
