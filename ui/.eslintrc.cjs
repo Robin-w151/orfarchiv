ee4f6377ee02dfd6f3b2b116be5f45c2fef9e89a
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2023-05-09 20:37:05 +0000

    adjust eslint config

--- ui/.eslintrc.cjs
+++ ui/.eslintrc.cjs
@@ -5,37 +5,11 @@ module.exports = {
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/no-unused-vars': 'warn',
     'no-unused-vars': 'off',
-    'svelte/no-at-html-tags': 'warn',
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:svelte/recommended',
-    'plugin:storybook/recommended',
-    'prettier',
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
   plugins: ['@typescript-eslint'],
   env: {
-    browser: true,
     es2022: true,
     node: true,
   },
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    sourceType: 'module',
-    ecmaVersion: 2022,
-    project: './tsconfig.json',
-    tsconfigRootDir: __dirname,
-    extraFileExtensions: ['.svelte'],
-  },
-  overrides: [
-    {
-      files: ['*.svelte'],
-      parser: 'svelte-eslint-parser',
-      parserOptions: {
-        parser: '@typescript-eslint/parser',
-      },
-    },
-  ],
-  ignorePatterns: ['.eslintrc.*', 'service-worker.ts'],
 };
