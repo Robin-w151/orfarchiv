f39884edf8ab3b31ccf0fbb58cb461cd013e72a4
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2023-12-19 19:22:41 +0000

    disable db init during prerender phase

--- ui/src/hooks.server.ts
+++ ui/src/hooks.server.ts
@@ -1,7 +1,10 @@
+import { building } from '$app/environment';
 import orfArchivDb from '$lib/backend/db/init';
 import type { Handle } from '@sveltejs/kit';
 
 export const handle = (async ({ event, resolve }) => {
-  await orfArchivDb.init();
+  if (!building) {
+    await orfArchivDb.init();
+  }
   return resolve(event);
 }) satisfies Handle;
