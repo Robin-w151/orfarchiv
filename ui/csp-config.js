c5d85ede37bbf1942ac0619d128577ab33775c98
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2024-04-15 19:45:30 +0000

    switch to vercel speed insights

--- ui/csp-config.js
+++ ui/csp-config.js
@@ -3,7 +3,7 @@ import cspScriptHashes from './csp-script-hashes.js';
 export default {
   directives: {
     'default-src': ['none'],
-    'connect-src': ['self', 'https://vitals.vercel-insights.com'],
+    'connect-src': ['self'],
     'font-src': ['self', 'data:'],
     'img-src': ['*', 'data:'],
     'script-src': ['self', ...cspScriptHashes],
--- ui/src/lib/components/utils/EnableAnalytics.svelte
+++ ui/src/lib/components/utils/EnableAnalytics.svelte
@@ -1,5 +1,6 @@
 <script lang="ts" context="module">
   import { inject } from '@vercel/analytics';
+  import { injectSpeedInsights } from '@vercel/speed-insights';
   import { browser } from '$app/environment';
   import { PUBLIC_ENABLE_ANALYTICS } from '$env/static/public';
 
@@ -9,20 +10,6 @@
 
   if (browser && isAnalyticsEnabled()) {
     inject();
-  }
-</script>
-
-<script lang="ts">
-  import { webVitals } from '$lib/utils/vitals';
-  import { page } from '$app/stores';
-
-  const analyticsId = import.meta.env.VERCEL_ANALYTICS_ID;
-  $: if (browser && isAnalyticsEnabled() && analyticsId) {
-    const { url, params } = $page;
-    webVitals({
-      path: url.pathname,
-      params,
-      analyticsId,
-    });
+    injectSpeedInsights();
   }
 </script>
