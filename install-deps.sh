43ce566ed5a2bb44902216722e1beecd921b6b4b
Robin Wunderbaldinger <robinwunderbaldinger@gmail.com>
2025-08-24 16:43:35 +0000

    add install-deps script

---
+++ install-deps.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+modules=(db scraper ui)
+
+for module in "${modules[@]}"; do
+    cd "${module}" || exit 1
+    echo "Installing dependencies for ${module}..."
+    npm install
+    echo "Done installing dependencies for ${module}."
+    cd .. || exit 1
+done
